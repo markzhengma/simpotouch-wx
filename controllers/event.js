4ca11105139bfdbd0a4f4023d85914bd19a5eef2
Mark Zheng Ma <mazheng1022@hotmail.com>
2020-01-29 13:43:34 +0800

    update api path

--- controllers/event.js
+++ controllers/event.js
@@ -3,7 +3,7 @@ const util = require('../utils/util');
 class EventController {
   getAllEvent(callback){
     util.request(
-      'https://api.simpotouch.com/api/v1/event/all',
+      'https://api.simpotouch.com/v1/event/all',
       {},
       'GET',
       callback
